ecf7c094579091d0ae736e7be73ea432d75eb921
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2022-02-03 14:25:35 +0700

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simaster Wallpaper Generator',
       theme: ThemeData(
         textTheme: TextTheme(button: TextStyle(fontFamily: 'Roboto')),
         // This is the theme of your application.
